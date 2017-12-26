0c70b66963c14f2b2febb8d3366de48587d7df20
Ljhhhhhh <13216698987@163.com>
2017-12-26 17:00:00 +0800

    chapter5

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,14 +1,24 @@
 export function hasClass(el, className) {
-  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
-  return reg.test(el.className)
+    let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+    return reg.test(el.className)
 }
 
 export function addClass(el, className) {
-  if (hasClass(el, className)) {
-    return
-  }
+    if (hasClass(el, className)) {
+        return
+    }
 
-  let newClass = el.className.split(' ')
-  newClass.push(className)
-  el.className = newClass.join(' ')
+    let newClass = el.className.split(' ')
+    newClass.push(className)
+    el.className = newClass.join(' ')
+}
+
+export function getData(el, name, val) {
+    const prefix = "data-"
+    name = prefix + name
+    if (val) {
+        return el.setAttribute(name, val)
+    } else {
+        return el.getAttribute(name)
+    }
 }
